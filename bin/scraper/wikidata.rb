163a3e4ddc1c1483f6b30e3c9f872a2cf568826e
Tony Bowden <tony@tmtm.com>
2021-06-30 14:36:43 +0300

    Fetch CSV directly from WDQS
    Rather than getting back JSON and post-processing it, fetch the result
    directly as CSV.

--- bin/scraper/wikidata.rb
+++ bin/scraper/wikidata.rb
@@ -5,40 +5,26 @@ require 'cgi'
 require 'csv'
 require 'scraped'
 
-class Results < Scraped::JSON
-  field :members do
-    json[:results][:bindings].map { |result| fragment(result => Member).to_h }
-  end
-end
-
-class Member < Scraped::JSON
-  field :name do
-    json.dig(:name, :value)
-  end
-end
-
-WIKIDATA_SPARQL_URL = 'https://query.wikidata.org/sparql?format=json&query=%s'
+WIKIDATA_SPARQL_URL = 'https://query.wikidata.org/sparql?query=%s'
 
 memberships_query = <<SPARQL
-  SELECT ?item ?name
+  SELECT (STRAFTER(STR(?member), STR(wd:)) AS ?item) ?name
   WHERE {
-    ?item p:P39 ?ps .
+    ?member p:P39 ?ps .
     ?ps ps:P39 wd:Q21294917 ; pq:P2937 wd:Q104845061 .
     FILTER NOT EXISTS { ?ps pq:P582 ?end }
 
     OPTIONAL { ?ps prov:wasDerivedFrom/pr:P1810 ?sourceName }
-    OPTIONAL { ?item rdfs:label ?enLabel FILTER(LANG(?enLabel) = "en") }
+    OPTIONAL { ?member rdfs:label ?enLabel FILTER(LANG(?enLabel) = "en") }
     BIND(COALESCE(?sourceName, ?enLabel) AS ?name)
   }
   ORDER BY ?name
 SPARQL
 
 url = WIKIDATA_SPARQL_URL % CGI.escape(memberships_query)
-headers = { 'User-Agent' => 'every-politican-scrapers/singapore-parliament-official' }
-data = Results.new(response: Scraped::Request.new(url: url, headers: headers).response).members
+headers = {
+  'User-Agent' => 'every-politican-scrapers/singapore-parliament-official',
+  'Accept' => 'text/csv',
+}
 
-header = data.first.keys.to_csv
-rows = data.map { |row| row.values.to_csv }
-abort 'No results' if rows.count.zero?
-
-puts header + rows.join
+puts Scraped::Request.new(url: url, headers: headers).response.body
