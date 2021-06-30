499d4d8142e9071f77ee5c7b2869243d4a96d598
Tony Bowden <tony@tmtm.com>
2021-06-30 09:54:00 +0300

    Look at 'named as' field in reference for source name
    The official site lists members by a fuller version of their name than
    is commonly used, so look for a "named as" field on the reference to
    compare with instead.
    
    (This will fail if there is more than one such "named as", e.g. across
    separate references for the same statement, but we can fix that kui it
    happens)

--- bin/scraper/wikidata.rb
+++ bin/scraper/wikidata.rb
@@ -13,25 +13,28 @@ end
 
 class Member < Scraped::JSON
   field :name do
-    json.dig(:itemLabel, :value)
+    json.dig(:name, :value)
   end
 end
 
 WIKIDATA_SPARQL_URL = 'https://query.wikidata.org/sparql?format=json&query=%s'
 
 memberships_query = <<SPARQL
-  SELECT ?item ?itemLabel
+  SELECT ?item ?name
   WHERE {
     ?item p:P39 ?ps .
     ?ps ps:P39 wd:Q21294917 ; pq:P2937 wd:Q104845061 .
     FILTER NOT EXISTS { ?ps pq:P582 ?end }
-    SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+
+    OPTIONAL { ?ps prov:wasDerivedFrom/pr:P1810 ?sourceName }
+    OPTIONAL { ?item rdfs:label ?enLabel FILTER(LANG(?enLabel) = "en") }
+    BIND(COALESCE(?sourceName, ?enLabel) AS ?name)
   }
-  ORDER BY ?itemLabel
+  ORDER BY ?name
 SPARQL
 
 url = WIKIDATA_SPARQL_URL % CGI.escape(memberships_query)
-headers = { 'User-Agent' => 'every-politican-scrapers/estonia-riigikogu' }
+headers = { 'User-Agent' => 'every-politican-scrapers/singapore-parliament-official' }
 data = Results.new(response: Scraped::Request.new(url: url, headers: headers).response).members
 
 header = data.first.keys.to_csv
